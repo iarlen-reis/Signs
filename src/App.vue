18546b7b44329d4584cb7af7572e15dd4cd70e8f
Iarlen <iarlensr@hotmail.com>
2023-08-30 21:23:19 -0300

    feat: commit initial.

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import HeaderVue from './components/Header.vue'
+</script>
+
+<template>
+  <HeaderVue />
+  <main class="main__content">
+    <RouterView />
+  </main>
+</template>
+
+<style lang="scss">
+@use './assets/scss/variables.scss' as *;
+@use './assets/scss/mixins.scss' as *;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+ul {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+body {
+  @include background;
+  font-family: $font-body;
+}
+
+.main__content {
+  width: 100%;
+  max-width: 1200px;
+  min-height: 100vh;
+  margin: 0 auto;
+  padding: 1rem;
+}
+</style>
