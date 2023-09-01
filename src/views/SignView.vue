cc1c9466a17d356b4bbdd662093c96bccbfdbef5
Iarlen <iarlensr@hotmail.com>
2023-09-01 18:34:06 -0300

    feat: installing tanStack (react-query) to make requests.

--- src/views/SignView.vue
+++ src/views/SignView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
 import { api } from '@/utils/api'
 import { useRoute } from 'vue-router'
-import { reactive } from 'vue'
-
+import { useQuery } from '@tanstack/vue-query'
+import { onMounted } from 'vue';
+import SignImage from '@/components/SignImage.vue';
+import SignDetails from '@/components/SignDetails.vue';
+import SignDescription from '@/components/SignDescription.vue';
+import SignCharacteristics from '@/components/SignCharacteristics.vue';
+import SignCompatility from '@/components/SignCompatility.vue';
 const route = useRoute()
 
 interface ISignProps {
@@ -17,56 +21,32 @@ interface ISignProps {
   }
 }
 
-const sign: ISignProps = reactive({} as ISignProps)
-
-onMounted(async () => {
+const {data: sign} = useQuery(['signs', route.params.id], async () => {
   const response = await api.get<ISignProps>(`signs/${route.params.id}`)
 
   const data = response.data
 
-  sign.sign = data.sign
+  return data
+}, {
+  staleTime: Infinity,
+})
 
+onMounted(() => {
   scrollTo(0, 0)
 })
 </script>
 
 <template>
-  <div class="sign" v-if="sign.sign">
-    <div class="sign__image">
-      <img :src="sign.sign.image" :alt="sign.sign.name" />
-    </div>
+  <div class="sign" v-if="sign">
+    <SignImage :image="sign.sign.image" />
     <div class="sign__information">
-      <div class="sign__details">
-        <h1>{{ sign.sign.name }}</h1>
-        <span>{{ sign.sign.period }}</span>
-      </div>
+      <SignDetails :name="sign.sign.name" :period="sign.sign.period" />
       <div class="sign_group__list">
-        <div class="sign__characteristics">
-          <h2>Caracteristicas:</h2>
-          <ul>
-            <li v-for="characteristic in sign.sign.characteristics" :key="characteristic">
-              - {{ characteristic }}
-            </li>
-          </ul>
-        </div>
-        <div class="sign__compatility">
-          <h2>Compatibilidade:</h2>
-          <ul>
-            <li v-for="compatility in sign.sign.compatility" :key="compatility">
-              - {{ compatility }}
-            </li>
-          </ul>
-        </div>
+        <SignCharacteristics :characteristics="sign.sign.characteristics" />
+        <SignCompatility :compatilitys="sign.sign.compatility" />
       </div>
       <div>
-        <div class="sign__description">
-          <h2>Sobre o signo:</h2>
-          <div class="sign__description__container">
-            <p v-for="description in sign.sign.description" :key="description">
-              {{ description }}
-            </p>
-          </div>
-        </div>
+        <SignDescription :descriptions="sign.sign.description" />
       </div>
     </div>
   </div>
@@ -79,7 +59,7 @@ onMounted(async () => {
 
 .sign {
   display: grid;
-  grid-template-rows: 1fr 2fr;
+  grid-template-rows: 1fr 2.5fr;
   gap: $gap-1;
 
   padding-bottom: 5rem;
@@ -87,52 +67,9 @@ onMounted(async () => {
   color: $color-white;
   text-shadow: 1px 1px 2px $color-link;
 
-  ul {
-    display: flex;
-    flex-direction: column;
-    gap: $gap-2;
-  }
-
-  li {
-    font-size: $size-lg;
-  }
-
-  @include tablet {
-    li {
-      font-size: $size-xl;
-    }
-  }
-
   @include desktop {
     grid-template-columns: 1fr 1.5fr;
     grid-template-rows: 1fr;
-  }
-}
-
-.sign__image {
-  width: 100%;
-  max-width: 300px;
-  max-height: fit-content;
-  margin: 0 auto;
-
-  img {
-    width: 100%;
-  }
-
-  @include desktop {
-    max-width: 400px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0;
-
-    img {
-      transition: all 0.4s;
-
-      &:hover {
-        transform: scale(1.1);
-      }
-    }
   }
 }
 
@@ -161,7 +98,7 @@ onMounted(async () => {
   flex-direction: column;
   gap: $gap-4;
 
-  @media (min-width: 460px) {
+  @media (min-width: 400px) {
     flex-direction: row;
     justify-content: space-between;
     margin: 0 auto;
@@ -169,62 +106,6 @@ onMounted(async () => {
 
   @include desktop {
     margin: 0;
-  }
-}
-
-.sign__details {
-  display: flex;
-  text-align: center;
-  flex-direction: column;
-
-  h1 {
-    font-size: $size-4xl;
-  }
-
-  span {
-    font-size: $size-xl;
-  }
-
-  @include tablet {
-    h1 {
-      font-size: $size-5xl;
-    }
-
-    span {
-      font-size: $size-2xl;
-    }
-  }
-
-  @include desktop {
-    text-align: start;
-  }
-}
-
-.sign__characteristics {
-  width: 100%;
-}
-
-.sign__description {
-  display: flex;
-  flex-direction: column;
-  gap: $gap-2;
-}
-
-.sign__description__container {
-  display: flex;
-  flex-direction: column;
-  gap: $gap-4;
-
-  p {
-    max-width: 700px;
-    font-size: $size-lg;
-  }
-
-  @include tablet {
-    p {
-      max-width: 800px;
-      font-size: $size-xl;
-    }
   }
 }
 </style>
--- src/views/SignsView.vue
+++ src/views/SignsView.vue
@@ -1,35 +1,28 @@
 <script setup lang="ts">
-  import SignCard from '@/components/SignCard.vue';
-  import {api} from '../utils/api';
-  import { onMounted, reactive } from 'vue';
+import SignCard from '@/components/SignCard.vue'
+import { api } from '../utils/api'
+import { useQuery } from '@tanstack/vue-query'
 
+interface ISignProps {
+  _id: string
+  name: string
+  image: string
+  period: string
+}
 
+interface ISignsProps {
+  signs: ISignProps[]
+}
 
-  interface ISignProps {
-    _id: string;
-    name: string;
-    image: string;
-    period: string;
-  }
+const { data: signs } = useQuery(['signs'], async () => {
+  const response = await api.get<ISignsProps>('/signs')
 
-  interface ISignsProps {
-    signs: ISignProps[];
-  }
+  const data = response.data
 
-
-
-  const signs: ISignProps[] = reactive([])
-
-
-  onMounted(async () => {
-    const response = await api.get<ISignsProps>('/signs');
-
-    const data = response.data;
-
-    signs.push(...data.signs);
-  });
-
-
+  return data
+}, {
+  staleTime: Infinity
+})
 </script>
 
 <template>
@@ -38,15 +31,16 @@
       <h1>signos Zodíacos!</h1>
       <p>Aqui se encontra todos os signos zodíacos.</p>
     </div>
-    <div class ="signs__grid">
+    <div class="signs__grid" v-if="signs">
       <SignCard
-       v-for="sign in signs" 
-       :key="sign._id"
-       :id="sign._id" 
-       :sign="sign" 
-       :image="sign.image" 
-       :name="sign.name"
-       :period="sign.period"/>
+        v-for="sign in signs.signs"
+        :key="sign._id"
+        :id="sign._id"
+        :sign="sign"
+        :image="sign.image"
+        :name="sign.name"
+        :period="sign.period"
+      />
     </div>
   </div>
 </template>
@@ -81,34 +75,33 @@
 
   @include tablet {
     h1 {
-    font-family: $font-title;
-    font-size: $size-4xl;
-  }
+      font-family: $font-title;
+      font-size: $size-4xl;
+    }
 
-
-  p {
-    font-size: $size-xl;
-  }
+    p {
+      font-size: $size-xl;
+    }
   }
 }
 
 .signs__grid {
-    display: grid;
-    grid-template-columns: 1fr;
-    gap: $gap-7;
-    justify-content: space-between;
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: $gap-7;
+  justify-content: space-between;
 
-    @include tablet {
-      grid-template-columns: repeat(2, 1fr);
-    }
+  @include tablet {
+    grid-template-columns: repeat(2, 1fr);
+  }
 
-    @include desktop {
-      grid-template-columns: repeat(3, 1fr);
-    }
+  @include desktop {
+    grid-template-columns: repeat(3, 1fr);
+  }
 
-    /* @media (min-width: 1280px) {
+  /* @media (min-width: 1280px) {
       grid-template-columns: repeat(4, 1fr);
       
     } */
-  }
+}
 </style>
